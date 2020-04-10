f3c5b4d39af3348a7178b2eb21f0236c06a9462e
Your Name <68rhaehfdlvn@gmail.com>
2020-04-10 18:34:17 +0900

    18:34

---
+++ grade/GradeSwing.java
@@ -0,0 +1,13 @@
+package com.jse.grade;
+
+import javax.swing.*;
+
+public class GradeSwing {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("학점계산기");
+		frame.setSize(800, 600);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
